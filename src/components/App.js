7e1d61f19086d263774981c025d90796d1027508
Tejesh Jidugu <tejeshjidugu@gmail.com>
2025-10-19 23:25:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import TodoList from "./TodoList";
 
 const App = () => {
     const [todos, setTodos] = useState([
