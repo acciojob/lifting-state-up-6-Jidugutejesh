3ac1bd40292f76a6f83c5126674682102f2673aa
Tejesh Jidugu <tejeshjidugu@gmail.com>
2025-10-19 23:20:02 +0530

    Create TodoList.js

---
+++ src/components/TodoList.js
@@ -0,0 +1,15 @@
+export default function TodoList({ data, changeData }) {
+  return (
+    <div>
+      <h2>Child Component</h2>
+      <ul>
+        {data.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}{" "}
+            {todo.completed ? null : ( <button onClick={() => changeData(todo.id)}>Complete</button> )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
