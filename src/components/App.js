52f8517befdba468254e1d70ecd37f0e52761031
Tejesh Jidugu <tejeshjidugu@gmail.com>
2025-10-19 23:17:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,24 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+    const [todos, setTodos] = useState([
+    { id: 1, text: "Do Homework", completed: false },
+    { id: 2, text: "Are you went home", completed: false },
+    { id: 3, text: "What are you doing", completed: false },
+  ]);
+
+  const handleComplete = (id) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: true } : todo
+    );
+    setTodos(updatedTodos);
+  };
   return (
     <div>
+      <div>
+      <h1>Parent Component</h1>
+      <TodoList data={todos} changeData={handleComplete} />
+    </div>
         {/* Do not remove the main div */}
     </div>
   )
