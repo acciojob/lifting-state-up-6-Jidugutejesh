3df0f94d7b47bb70522fe941ea71de621ed2ffb3
Tejesh Jidugu <tejeshjidugu@gmail.com>
2025-10-20 12:55:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,21 @@
 import React, { useState } from "react";
 import './../styles/App.css';
 import TodoList from "./TodoList";
-
-const App = () => {
   const [todos, setTodos] = useState([
     { id: 1, text: "Do Homework", completed: false },
     { id: 2, text: "Are you went home", completed: false },
     { id: 3, text: "What are you doing", completed: false },
   ]);
 
-  // Update the todo as completed
+  // Function to mark a specific todo as completed
   const handleComplete = (id) => {
-    const updated = todos.map((todo) =>
+    const updatedTodos = todos.map((todo) =>
       todo.id === id ? { ...todo, completed: true } : todo
     );
-    setTodos(updated);
+    setTodos(updatedTodos);
   };
   return (
-     <div>
+    <div>
       <h1>Parent Component</h1>
       <TodoList data={todos} changedata={handleComplete} />
     </div>
